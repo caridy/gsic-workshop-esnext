905dfba9ad5d2a46f9a0a2bf089e0fc00ffced76
Caridy Patino <caridy@gmail.com>
2014-10-29 07:23:07 -0400

    making the grunt file more generic

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,19 @@
 
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    esnext: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        src: ['src/main.js'],
-        dest: 'dist/main.js'
-      }
-    }
-  });
+    grunt.initConfig({
+        esnext: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/main.js'
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-esnext');
-  grunt.registerTask('default', ['esnext']);
+    grunt.loadNpmTasks('grunt-esnext');
+    grunt.registerTask('default', ['esnext']);
 
 };
